90c5e56a819bbfaab7ef8f4c5f615ab81d6d6895
Samuel Moraes <samlucas2608@gmail.com>
2024-08-19 20:13:04 -0300

    Arrumando Cadastro

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,10 +1,12 @@
-import { Card, CardHeader, FormControl, FormLabel, Input, Button, Link, CardContent, Typography, CardMedia, Grid, Paper, TextField } from "@mui/material";
+import { Card, CardHeader, FormControl, FormLabel, Input, Button, CardContent, Typography, CardMedia, Grid, Paper, TextField } from "@mui/material";
 import "./styles.css"
 import Logo from "/src/assets/matchmaking-logo-reduzido.png"
 import { Field, Form, Formik, ErrorMessage } from "formik";
 import * as Yup from 'yup'
 import api from "../../services/api";
 import { useNavigate } from 'react-router-dom';
+import Sidebar from "../../components/Sidebar";
+import { Link } from "react-router-dom";
 
 
 const initialValues = {
@@ -31,118 +33,76 @@ export default function Cadastro() {
     async function onSubmit(values, props) {
         console.log('SUBMIT', values);
         props.resetForm();
-    
+
         try {
             const response = await api.post('/auth/register', values);
-            navigate('/');
+            navigate('/login');
         } catch (error) {
             alert("Não foi possível fazer o cadastro. Favor tentar novamente");
         }
     }
 
     return (
-        <Card
-            sx={{ margin: "0em 20em 0em" }}
-            className="card-form">
-            <Grid container direction="row"
-                justifyContent="center"
-            >
-                <CardMedia
-                    className="img"
-                    sx={{ width: "500px", height: "100px", borderRadius: '200px 200px 200px 200px' }}
-                    image={Logo}
-                />
-            </Grid>
-            <Typography variant="h2" color="text.secondary" gutterBottom component="div" align="center">
-                Cadastro de Usuário
-            </Typography>
-            <CardContent>
-                <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-                    {(props) => (
-                        <Form >
-                            <Field
-                                sx={{ margin: '1em' }}
-                                as={TextField} label="Nome de usuário" name="username"
-                                placeholder='Nome de usuário' fullWidth required
-                                helperText={<ErrorMessage name="username" />}
-                            />
-                            <Field
-                                sx={{ margin: '1em' }}
-                                as={TextField} label="Nome" name="name"
-                                placeholder='Nome' fullWidth required
-                                helperText={<ErrorMessage name="name" />}
-                            />
-                            <Field
-                                sx={{ margin: '1em' }}
-                                as={TextField} label="E-mail" name="email"
-                                placeholder='E-mail' fullWidth required
-                                helperText={<ErrorMessage name="email" />}
-                            />
-                            <Field
-                                sx={{ margin: '1em' }}
-                                as={TextField} label='Senha' name="password"
-                                placeholder='Senha' type='password' fullWidth required
-                                helperText={<ErrorMessage name="password" />}
-                            />
-                            <div className='button'>
-                                <Button type='submit' color='primary' variant="contained" disabled={props.isSubmitting}
-                                    style={btnstyle} fullWidth>{props.isSubmitting ? "Carregando" : "Cadastrar"}</Button>
-                                {/* <Link className='link' to="/register"><Button colorscheme='blue' >Cadastrar-se</Button></Link> */}
-                            </div>
-                        </Form>
-                    )}
+        < div className='container' >
+            <Sidebar />
+                <Card
+                    // sx={{ margin: "0em 0em 0em" }}
+                    className="card-sobrenos">
+                    <Grid container direction="row"
+                        justifyContent="center"
+                    >
+                        <CardMedia
+                            className="img"
+                            sx={{ width: "500px", height: "200px", borderRadius: '200px 200px 200px 200px' }}
+                            image={Logo}
+                        />
+                    </Grid>
+                    <Typography variant="h2" color="text.secondary" gutterBottom component="div" align="center">
+                        Cadastro de Usuário
+                    </Typography>
+                    <CardContent>
+                        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+                            {(props) => (
+                                <Form >
+                                    <Field
+                                        sx={{ margin: '1em' }}
+                                        as={TextField} label="Nome de usuário" name="username"
+                                        placeholder='Nome de usuário' fullWidth required
+                                        helperText={<ErrorMessage name="username" />}
+                                    />
+                                    <Field
+                                        sx={{ margin: '1em' }}
+                                        as={TextField} label="Nome" name="name"
+                                        placeholder='Nome' fullWidth required
+                                        helperText={<ErrorMessage name="name" />}
+                                    />
+                                    <Field
+                                        sx={{ margin: '1em' }}
+                                        as={TextField} label="E-mail" name="email"
+                                        placeholder='E-mail' fullWidth required
+                                        helperText={<ErrorMessage name="email" />}
+                                    />
+                                    <Field
+                                        sx={{ margin: '1em' }}
+                                        as={TextField} label='Senha' name="password"
+                                        placeholder='Senha' type='password' fullWidth required
+                                        helperText={<ErrorMessage name="password" />}
+                                    />
+                                    <div className='button'>
+                                        <Button type='submit' color='primary' variant="contained" disabled={props.isSubmitting}
+                                            style={btnstyle} fullWidth>{props.isSubmitting ? "Carregando" : "Cadastrar"}</Button>
+                                        {/* <Link className='link' to="/register"><Button colorscheme='blue' >Cadastrar-se</Button></Link> */}
+                                    </div>
+                                    <div className="w-100% text-center">
+                                        <p>Já possui cadastro? <Link className="text-sky-400" to="/login">Fazer login</Link>  </p>
+                                    </div>
+                                </Form>
+                            )}
 
-                </Formik>
+                        </Formik>
 
-                {/* <form>
-                        <section className="form">
-                            <div className='form-input'>
-                                <FormControl
-                                    sx={{ margin: '1em 1em 2em'}}
-                                >
-                                    <FormLabel>Nome de Usuário:</FormLabel>
-                                    <Input
-                                        isrequired="true"
-                                        onChange={e => setUsername(e.target.value)}
-                                        placeholder='Usuário' />
-                                </FormControl>
-                                <FormControl
-                                sx={{ margin: '1em 1em 2em'}}>
-                                    <FormLabel>Nome:</FormLabel>
-
-                                    <Input
-                                        isrequired="true"
-                                        onChange={e => setUsername(e.target.value)}
-                                        placeholder='Nome' />
-                                </FormControl>
-                                <FormControl
-                                sx={{ margin: '1em 1em 2em'}}>
-                                    <FormLabel>E-mail:</FormLabel>
-
-                                    <Input
-                                        type="email"
-                                        isrequired="true"
-                                        onChange={e => setUsername(e.target.value)}
-                                        placeholder='usuario@email.com' />
-                                </FormControl>
-                                <FormControl
-                                sx={{ margin: '1em 1em 2em'}}>
-                                    <FormLabel>Senha:</FormLabel>
-
-                                    <Input
-                                        isrequired="true"
-                                        onChange={e => setUsername(e.target.value)}
-                                        type="password"
-                                        placeholder='Senha' />
-                                </FormControl>
-                            </div>
-                            <div className='button'>
-                                <Button  sx={{ margin: '1em 1em 2em'}} type='submit' color="inherit" >Login</Button>
-                                <Link className='link' to="/register"><Button colorscheme='blue' >Cadastrar-se</Button></Link>
-                            </div>
-                        </section>
-                    </form> */}
-            </CardContent>
-        </Card>
+                    </CardContent>
+                </Card>
+        </div>
     )
 }
