2fb1e3ea82d3f4544e2453f7e493cca7033563fd
Samuel Moraes <samlucas2608@gmail.com>
2024-08-19 20:12:49 -0300

    Arrumando Login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -6,6 +6,7 @@ import * as Yup from 'yup'
 import api from "../../services/api";
 import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
+import Sidebar from "../../components/Sidebar";
 
 
 
@@ -26,71 +27,68 @@ const validationSchema = Yup.object().shape({
 export default function Login() {
 
     const navigate = useNavigate();
-    
+
     async function onSubmit(values, props) {
 
-        // const data = {
-        //     username: username,
-        //     password: password
-        // };
-    
-        // try {
-            const response = await api.post('/auth', values);
-    
-            localStorage.setItem('username', values.username);
-            localStorage.setItem('token', response.data.token);
-    
-            navigate('/welcome')
-        // } catch (error) {
-            // alert("Não foi possível fazer login. Favor tentar novamente");
-        // }
-        console.log('SUBMIT', values);
-        // props.resetForm();
-        // props.setSubmitting(false);
+        try {
+        const response = await api.post('/auth', values);
+
+        localStorage.setItem('username', values.username);
+        localStorage.setItem('token', response.data.token);
+
+        navigate('/postar')
+        } catch (error) {
+            alert("Não foi possível fazer login. Favor tentar novamente");
+        }
     }
 
     return (
-        <Card
-            sx={{ margin: "0em 20em 0em" }}
-            className="card-form">
-            <Grid container direction="row"
-                justifyContent="center"
-            >
-                <CardMedia
-                    className="img"
-                    sx={{ width: "500px", height: "100px", borderRadius: '200px 200px 200px 200px' }}
-                    image={Logo}
-                />
-            </Grid>
-            <Typography variant="h2" color="text.secondary" gutterBottom component="div" align="center">
-                Login
-            </Typography>
-            <CardContent>
-                <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-                    {(props) => (
-                        <Form >
-                            <Field
-                                sx={{ margin: '1em' }}
-                                as={TextField} label="Nome de usuário" name="username"
-                                placeholder='Nome de usuário' fullWidth required
-                                helperText={<ErrorMessage name="username" />}
-                            />
-                            <Field
-                                sx={{ margin: '1em' }}
-                                as={TextField} label='Senha' name="password"
-                                placeholder='Senha' type='password' fullWidth required
-                                helperText={<ErrorMessage name="password" />}
-                            />
-                            <div className='button'>
-                                <Button type='submit' color='primary' variant="contained" disabled={props.isSubmitting}
-                                    style={btnstyle} fullWidth>{props.isSubmitting ? "Carregando" : "Login"}</Button>
-                                <Link className='link' to="/register"><Button color="success" variant="contained">Cadastrar-se</Button></Link>
-                            </div>
-                        </Form>
-                    )}
+        < div className='container' >
+            <Sidebar />
+            <div>
+                <Card
+                    sx={{ margin: "0em 0em 0em" }}
+                    className="card-form">
+                    <Grid container direction="row"
+                        justifyContent="center"
+                    >
+                        <CardMedia
+                            className="img"
+                            sx={{ width: "500px", height: "200px", borderRadius: '200px 200px 200px 200px' }}
+                            image={Logo}
+                        />
+                    </Grid>
+                    <Typography variant="h2" color="text.secondary" gutterBottom component="div" align="center">
+                        Login
+                    </Typography>
+                    <CardContent className="content-login">
+                        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+                            {(props) => (
+                                <Form >
+                                    <Field
+                                        sx={{ margin: '1em' }}
+                                        as={TextField} label="Nome de usuário" name="username"
+                                        placeholder='Nome de usuário' fullWidth required
+                                        helperText={<ErrorMessage name="username" />}
+                                    />
+                                    <Field
+                                        sx={{ margin: '1em' }}
+                                        as={TextField} label='Senha' name="password"
+                                        placeholder='Senha' type='password' fullWidth required
+                                        helperText={<ErrorMessage name="password" />}
+                                    />
+                                    <div className='button'>
+                                        <Button className="h-12" type='submit' color='primary' variant="contained" disabled={props.isSubmitting}
+                                            style={btnstyle} fullWidth>{props.isSubmitting ? "Carregando" : "Login"}</Button>
+                                        <Link className='link' to="/cadastrar"><Button className="h-12" color="success" variant="contained">Cadastrar-se</Button></Link>
+                                    </div>
+                                </Form>
+                            )}
 
-                </Formik>
-            </CardContent>
-        </Card>
+                        </Formik>
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
     )
 }
