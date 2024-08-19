ac399651268f8fa7c9f4c864bef09a41c1206c9f
Samuel Moraes <samlucas2608@gmail.com>
2024-08-19 20:13:43 -0300

    Adicionando novas rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,6 +6,8 @@ import Posts from "./pages/Posts";
 import Header from "./components/Header";
 import InitialPage from "./pages/InicialPage";
 import Postar from "./pages/Postar";
+import SobreNos from "./pages/SobreNos";
+import EditarPost from "./pages/EditarPost";
 
 export default function AppRoutes() {
     return (
@@ -14,10 +16,12 @@ export default function AppRoutes() {
                 {/* <Route path="/" element={<Login/>}/> */}
                 <Route path="/" element={<InitialPage/>}/>
                 <Route path="/postar" element={<Postar/>}/>
-                <Route path="/register" element={<Cadastro/>}/>
+                <Route path="/cadastrar" element={<Cadastro/>}/>
+                <Route path="/login" element={<Login/>}/>
                 <Route path="/welcome" element={<Welcome/>}/>
                 <Route path="/posts/:id" element={<Posts/>}/>
-                <Route path="/header" element={<Header/>}/>
+                <Route path="/posts/:id/edit" element={<EditarPost/>}/>
+                <Route path="/sobreNos" element={<SobreNos/>}/>
             </Routes>
         </BrowserRouter>
     )
